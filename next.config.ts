e812f64f6db8d7a90974216bc2de3339effde02d
Shrikant Kshirsagar <shrikantkshirsagar29@gmail.com>
2025-03-26 18:36:11 -0400

    keep eslint for production builds as it doesn't create any build issues anymore

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // This will disable ESLint checking during production builds
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
