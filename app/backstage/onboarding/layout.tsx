20cb1bf63d8bcbce27778eaf8ea3aa221129f455
Shrikant Kshirsagar <shrikantkshirsagar29@gmail.com>
2025-05-06 23:33:29 -0400

    Revert "remove layout page"
    This reverts commit 7ac4c924dc9f84e7a0d2bd1f7a148ad5dc988e09.

---
+++ app/backstage/onboarding/layout.tsx
@@ -0,0 +1,10 @@
+// app/profile/layout.tsx
+import Layout from "../layouts/layout";
+
+export default function DashboardSectionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Layout>{children}</Layout>;
+}
