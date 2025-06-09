4f22f19382916a1a03ca18944867f0a3ec013037
Jaycoder7 <jyotil.yatin2006@gmail.com>
2025-06-09 13:18:05 -0400

    Replaced pin with dog tags

--- components/qbo/qbo-circles.tsx
+++ components/qbo/qbo-circles.tsx
@@ -181,18 +181,12 @@ export function QBOCircles({ onSelectJob }: QBOCircleProps) {
                                 key={job._id}
                                 className="inline-flex items-center justify-center p-1"
                               >
-                                <svg
-                                  className="h-4 w-4"
-                                  viewBox="0 0 24 24"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  strokeWidth="2"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                >
-                                  <path d="M10 13a3 3 0 0 1-3-3V5a3 3 0 0 1 6 0v5a3 3 0 0 1-3 3z" />
-                                  <path d="M10 13v3m-4-3h8" />
-                                </svg>
+                                <img
+                                  src="/dogtag.png"
+                                  alt="Job"
+                                  className="h-5 w-5 flex-shrink-0 object-contain"
+                                />
+
                               </span>
                             ))}
                           </div>
@@ -227,18 +221,12 @@ export function QBOCircles({ onSelectJob }: QBOCircleProps) {
                                   if (onSelectJob) onSelectJob(job._id);
                                 }}
                               >
-                                <svg
-                                  className="h-5 w-5 flex-shrink-0"
-                                  viewBox="0 0 24 24"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  strokeWidth="2"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                >
-                                  <path d="M10 13a3 3 0 0 1-3-3V5a3 3 0 0 1 6 0v5a3 3 0 0 1-3 3z" />
-                                  <path d="M10 13v3m-4-3h8" />
-                                </svg>
+                               <img
+                                  src="/dogtag.png"
+                                  alt="Job"
+                                  className="h-5 w-5 flex-shrink-0 object-contain"
+                                />
+
                                 <span className="hover:underline cursor-pointer">
                                   {job.title}
                                 </span>
